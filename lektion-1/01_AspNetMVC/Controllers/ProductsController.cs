89c29829c51dc3c5cd636d021f3abe3eccee1b6d
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2022-01-11 16:07:18 +0100

    lektion 1 (ASP.NET MVC, Microsoft Balzor intro)

---
+++ lektion-1/01_AspNetMVC/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using _01_AspNetMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace _01_AspNetMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var product = new ProductModel() { Id = 1, Name = "Product 1", Description = "En beskrivning", Price = 100 };
+            var products = new List<ProductModel>();
+            products.Add(product);
+            
+            return View(products);
+        }
+
+        public IActionResult Details()
+        {
+            var product = new ProductModel() { Id = 1, Name = "Product 1", Description = "En beskrivning", Price = 100 };
+
+            return View(product);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ lektion-1/01_AspNetMVC/Models/ProductsIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_AspNetMVC.Models
+{
+    public class ProductsIndexViewModel
+    {
+        public List<ProductModel> Products { get; set; }
+        public List<ProductModel> Latest { get; set; }
+        public List<ProductModel> Highlights { get; set; }
+
+    }
+}
